9430ef8571370e8b90992e1cd952bc1d4b1a8d72
Daniel Miao <rbmiao@gmail.com>
2021-01-20 18:07:13 -0500

    Create a web server

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+	default = "us-east-1"
+}
+
+variable "web_server_port" {
+	description = "Port the server will use for HTTP requests server"
+	default = "80"
+}
+
+variable "ssh_port" {
+	description = "Port the server will use for SSH login"
+	default = "22"
+}
